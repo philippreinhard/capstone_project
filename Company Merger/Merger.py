9c42a2892b008bab41f6778cfd06b8a2a8fc30cf
Skypsycher <philipparthur.fanschlaeder@stud.tu-darmstadt.de>
2020-06-19 00:21:40 +0200

    added a merger script that merges 2 company_attribute.json files into one

---
+++ Company Merger/Merger.py
@@ -0,0 +1,70 @@
+import json
+
+with open('input/input1.json', 'r') as input1:
+    list1 = json.load(input1)
+
+with open('input/input2.json', 'r') as input2:
+    list2 = json.load(input2)
+
+output_list = []
+
+
+for company1 in list1:
+    found = False
+    print('Company1: ' + str(company1))
+    for company2 in list2:
+        if company1 == company2:
+            print('Company2: ' +str(company2))
+            output_list.append(company1)
+            found = True
+            break
+        if company1[0] == company2[0]:
+            temp_list = []
+            temp_list.append(company1[0])
+
+            if company1[1] == 'missing' and company2[1] != 'missing':
+                temp_list.append(company2[1])
+            else:
+                temp_list.append(company1[1])
+
+            if company1[2] == 'missing' and company2[2] != 'missing':
+                temp_list.append(company2[2])
+            else:
+                temp_list.append(company1[2])
+
+            if company1[3] == 'missing' and company2[3] != 'missing':
+                temp_list.append(company2[3])
+            else:
+                temp_list.append(company1[3])
+
+            if company1[4] == 'missing' and company2[4] != 'missing':
+                temp_list.append(company2[4])
+            else:
+                temp_list.append(company1[4])
+            print('TempCompany Output: ' +str(temp_list))
+            output_list.append(temp_list)
+            found = True
+
+    ## list2 does not contain company from list1
+    if not found:
+        print('Not found in company2, adding to output:' + str(company1))
+        output_list.append(company1)
+
+
+
+
+def contains(alist, elem):
+    for l in alist:
+        if l[0] == elem[0]:
+            return True
+    return False
+
+
+for company2 in list2:
+    if not contains(output_list, company2):
+        output_list.append(company2)
+        print(company2)
+
+
+with open('output/output.json', 'w') as f:
+    json.dump(output_list, f)
